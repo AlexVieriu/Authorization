178edb214a25c8c698ce75e708aeaf5fd40509ee
Your Name <you@example.com>
2021-08-07 05:50:00 +0300

    Add project files.

---
+++ ResourceBasedAuth/Data/DocumentRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ResourceBasedAuth.Models;
+
+namespace ResourceBasedAuth.Data
+{
+    public class DocumentRepository : IDocumentRepository
+    {
+        public Document Find(Guid documentId)
+        {
+            return new Document
+            {
+                Author = "john.doe@microsoft.com",
+                Content = null,
+                ID = documentId,
+                Title = "Test Document"
+            };
+        }
+    }
+
+    public interface IDocumentRepository
+    {
+        Document Find(Guid documentId);
+    }
+}
